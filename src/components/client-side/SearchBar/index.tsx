41dde0c205d0d42a32ecde4d3f6376bde40d1b8b
Felipe Gangrel <felipefrog@gmail.com>
2023-11-25 14:09:09 -0300

    Refactor SearchBar component

--- src/components/client-side/SearchBar/index.tsx
+++ src/components/client-side/SearchBar/index.tsx
@@ -1,43 +1,43 @@
-import { cn } from "@/libs/styles";
-import { debounce } from "lodash";
-import type { ChangeEvent, FC, InputHTMLAttributes } from "react";
-import { useCallback, useRef, useState } from "react";
-import { MagnifyingGlass, X } from "@phosphor-icons/react";
+import { cn } from '@/libs/styles'
+import { debounce } from 'lodash'
+import type { ChangeEvent, FC, InputHTMLAttributes } from 'react'
+import { useCallback, useRef, useState } from 'react'
+import { MagnifyingGlass, X } from '@phosphor-icons/react'
 
-type Props = InputHTMLAttributes<HTMLInputElement>;
+type Props = InputHTMLAttributes<HTMLInputElement>
 
 export const SearchBar: FC<Props> = ({ className, onChange, ...props }) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('')
 
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const clearSearch = useCallback(() => {
-    setSearch("");
-    inputRef?.current?.focus();
-  }, []);
+    setSearch('')
+    inputRef?.current?.focus()
+  }, [])
 
   const handleChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
-      onChange?.(e);
+      onChange?.(e)
     },
     [onChange]
-  );
+  )
 
-  const handleDebouncedChange = debounce(handleChange, 500);
+  const handleDebouncedChange = debounce(handleChange, 500)
 
   return (
-    <div className="relative w-full flex flex-row items-center">
+    <div className="relative flex w-full flex-row items-center">
       <input
         {...props}
         ref={inputRef}
         type="text"
         className={cn(
-          "border border-slate-200 bg-gray-50 rounded-md pl-4 pr-10 py-2 w-full outline-none focus:ring-2 focus:ring-slate-500",
+          'w-full rounded-md border border-slate-200 bg-gray-50 py-2 pl-4 pr-10 outline-none focus:ring-2 focus:ring-slate-500',
           className
         )}
         onChange={(e) => {
-          handleDebouncedChange(e);
-          setSearch(e.target.value);
+          handleDebouncedChange(e)
+          setSearch(e.target.value)
         }}
         value={search}
         placeholder="Buscar..."
@@ -46,11 +46,11 @@ export const SearchBar: FC<Props> = ({ className, onChange, ...props }) => {
         type="button"
         className="absolute right-4"
         onClick={() => {
-          search && clearSearch();
+          search && clearSearch()
         }}
       >
         {search ? <X /> : <MagnifyingGlass />}
       </button>
     </div>
-  );
-};
+  )
+}
