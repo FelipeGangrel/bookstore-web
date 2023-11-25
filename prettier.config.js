353243540bc370aeed3d4f6fb3a7568abe5ce16f
Felipe Gangrel <felipefrog@gmail.com>
2023-11-25 13:59:04 -0300

    Add Prettier configuration and VS Code settings for formatting

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports  = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['cva', 'clsx', 'tw', 'twMerge', 'cn'],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+}
