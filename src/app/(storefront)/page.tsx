ad4afdcea7d226bbdd69fa30a7a29e9d31175f96
Felipe Gangrel <felipefrog@gmail.com>
2023-11-26 16:57:58 -0300

    Add ClientCard component to Home page

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -1,10 +1,17 @@
-import { BannersCarousel } from './_components/home'
+import { getServerSession } from '@/libs/session'
 
-export default function Home() {
+import { BannersCarousel, ClientCard } from './_components/home'
+
+export default async function Home() {
+  const session = await getServerSession()
+
+  console.log('server side session', session)
+
   return (
     <main className="h-[2000px] bg-slate-300">
       <section className="bg-gray-900 pt-20">
         <BannersCarousel />
+        <ClientCard />
       </section>
     </main>
   )
