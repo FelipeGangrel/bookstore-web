592021b281fcc86e81a9c16ffbdab37694190220
Felipe Gangrel <felipefrog@gmail.com>
2023-11-26 19:06:15 -0300

    Remove unused code and component

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -1,17 +1,10 @@
-import { getServerSession } from '@/libs/session'
+import { BannersCarousel } from './_components/home'
 
-import { BannersCarousel, ClientCard } from './_components/home'
-
-export default async function Home() {
-  const session = await getServerSession()
-
-  console.log('server side session', session)
-
+export default function Home() {
   return (
     <main className="h-[2000px] bg-slate-300">
       <section className="bg-gray-900 pt-20">
         <BannersCarousel />
-        <ClientCard />
       </section>
     </main>
   )
