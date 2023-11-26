a53da2d2fbd03450d89eef59a783425806f18b3a
Felipe Gangrel <felipefrog@gmail.com>
2023-11-26 20:26:18 -0300

    Refactor authOptions in auth.ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,44 +1,23 @@
 import type { AuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-import { FetchClient } from '@/libs/api-fetcher'
-
 export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: 'credentials',
       credentials: {
-        email: {},
-        password: {},
-        role: {},
+        user: {},
+        jwt: {},
       },
       authorize: async (credentials) => {
-        const fetchClient = new FetchClient()
-
-        try {
-          const url =
-            credentials?.role == 'admin'
-              ? '/auth/admin-login'
-              : '/auth/client-login'
-
-          const response = await fetchClient.post(url, {
-            email: credentials?.email,
-            password: credentials?.password,
-          })
-
-          const data = await response.json()
-
-          if (data?.user) {
-            return {
-              ...data.user,
-              jwt: data.token,
-            }
+        if (credentials?.user) {
+          return {
+            ...JSON.parse(credentials.user),
+            jwt: credentials?.jwt,
           }
-
-          return null
-        } catch (error: any) {
-          return null
         }
+
+        return null
       },
     }),
   ],
