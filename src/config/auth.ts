cb17c9e2e600a822dc67eac7bb36b2aaea99ff9e
Felipe Gangrel <felipefrog@gmail.com>
2023-11-26 16:53:44 -0300

    Add authentication configuration file

---
+++ src/config/auth.ts
@@ -0,0 +1,61 @@
+import type { AuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+import { FetchClient } from '@/libs/api-fetcher'
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: {},
+        password: {},
+        role: {},
+      },
+      authorize: async (credentials) => {
+        const fetchClient = new FetchClient()
+
+        try {
+          const url =
+            credentials?.role == 'admin'
+              ? '/auth/admin-login'
+              : '/auth/client-login'
+
+          const response = await fetchClient.post(url, {
+            email: credentials?.email,
+            password: credentials?.password,
+          })
+
+          const data = await response.json()
+
+          if (data?.user) {
+            return {
+              ...data.user,
+              jwt: data.token,
+            }
+          }
+
+          return null
+        } catch (error: any) {
+          return null
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user
+      }
+
+      return token
+    },
+    session: async ({ session, token }) => {
+      session = token.user as any
+      return session
+    },
+  },
+}
