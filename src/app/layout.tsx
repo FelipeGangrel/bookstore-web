ff30d0dc437300155d121e819c108373fa6886dd
Felipe Gangrel <felipefrog@gmail.com>
2023-11-26 16:57:24 -0300

    Add NextSessionProvider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { NextSessionProvider } from '@/providers'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,7 +19,9 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextSessionProvider>{children}</NextSessionProvider>
+      </body>
     </html>
   )
 }
