e69b6f6ecf540f1e28020d0d26f52d1989787ecd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-02 18:23:04 -0300

    Add ToastContainer component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import 'react-toastify/dist/ReactToastify.css'
 import './globals.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ToastContainer } from 'react-toastify'
 
 import { NextSessionProvider } from '@/providers'
 
@@ -20,7 +22,10 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextSessionProvider>{children}</NextSessionProvider>
+        <NextSessionProvider>
+          {children}
+          <ToastContainer position="top-right" />
+        </NextSessionProvider>
       </body>
     </html>
   )
