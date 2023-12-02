8d81f3adc8ef6cbc89f1e41577d3e27999ab2bc7
Felipe Gangrel <felipefrog@gmail.com>
2023-12-02 18:22:44 -0300

    Update button and link styles

--- src/styles/buttons.ts
+++ src/styles/buttons.ts
@@ -6,7 +6,7 @@ export const button = tv({
     color: {
       primary: 'border-black bg-black text-white',
       secondary: 'border-gray-300 bg-white text-black',
-      danger: 'border-red-500 bg-red-500 text-white',
+      danger: 'border-rose-500 bg-rose-500 text-white',
     },
     size: {
       sm: 'text-sm',
@@ -29,12 +29,12 @@ export const button = tv({
 export type ButtonVariants = VariantProps<typeof button>
 
 export const link = tv({
-  base: 'hover:underline active:opacity-80',
+  base: 'w-fit hover:underline active:opacity-80',
   variants: {
     color: {
       primary: 'hover:text-gray-890 text-gray-700',
       secondary: 'text-gray-400 hover:text-gray-500',
-      danger: 'text-red-500 hover:text-red-300',
+      danger: 'text-rose-500 hover:text-rose-300',
     },
     size: {
       sm: 'text-sm',
