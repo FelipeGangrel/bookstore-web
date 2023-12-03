9afcf3cf03971e9b384216076fc60b17d8279cf6
Felipe Gangrel <felipefrog@gmail.com>
2023-12-02 23:54:57 -0300

    Refactor carousel styling in storefront page

--- src/app/(storefront)/page.tsx
+++ src/app/(storefront)/page.tsx
@@ -5,7 +5,7 @@ import { Carousel } from '@/components/client-side'
 const Slide: FC<HTMLAttributes<HTMLDivElement>> = ({ children, ...props }) => (
   <div
     {...props}
-    className="flex h-96 w-full shrink-0 items-center justify-center bg-gray-800 text-white"
+    className="flex h-[600px] w-full shrink-0 items-center justify-center text-white"
   >
     {children}
   </div>
@@ -13,20 +13,22 @@ const Slide: FC<HTMLAttributes<HTMLDivElement>> = ({ children, ...props }) => (
 
 export default function Home() {
   return (
-    <div className="bg-slate-300">
+    <>
       <section className="bg-gray-900">
-        <Carousel autoSlide autoSlideInterval={5000}>
-          <Slide>
-            <h1 className="text-6xl font-bold text-yellow-300">Slide 1</h1>
-          </Slide>
-          <Slide>
-            <h1 className="text-6xl font-bold text-blue-300">Slide 2</h1>
-          </Slide>
-          <Slide>
-            <h1 className="text-6xl font-bold text-pink-300">Slide 3</h1>
-          </Slide>
-        </Carousel>
+        <div className="container mx-auto">
+          <Carousel autoSlide autoSlideInterval={5000}>
+            <Slide>
+              <h1 className="text-6xl font-bold text-yellow-300">Slide 1</h1>
+            </Slide>
+            <Slide>
+              <h1 className="text-6xl font-bold text-blue-300">Slide 2</h1>
+            </Slide>
+            <Slide>
+              <h1 className="text-6xl font-bold text-pink-300">Slide 3</h1>
+            </Slide>
+          </Carousel>
+        </div>
       </section>
-    </div>
+    </>
   )
 }
