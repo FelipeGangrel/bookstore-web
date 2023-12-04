5ed8bfa05bdfac70f454285976e074b22947a6d9
Felipe Gangrel <felipefrog@gmail.com>
2023-12-04 19:46:50 -0300

    Add create genre dialog and update page header

--- src/app/dashboard/(dashboard)/genres/page.tsx
+++ src/app/dashboard/(dashboard)/genres/page.tsx
@@ -1,14 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+
+import { CreateGenreDialog } from '@/components/dashboard/pages/genres'
 import { PageHeader } from '@/components/dashboard/shared'
-import { frontend } from '@/libs/navigation'
 
 export default function ListGenrePage() {
+  const [dialogOpen, setDialogOpen] = useState(false)
+
   return (
     <>
       <PageHeader
         title="Gêneros de livros"
         description="Gerencie gêneros de livros"
-        addActionUrl={frontend.dashboard.genres.create()}
+        addAction={() => setDialogOpen(true)}
         addActionLabel="Novo gênero"
+      />
+      <CreateGenreDialog
+        dialogOpen={dialogOpen}
+        onDialogOpenChange={setDialogOpen}
       />
     </>
   )
