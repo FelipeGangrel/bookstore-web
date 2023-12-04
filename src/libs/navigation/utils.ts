f484c7a55ec0bbf608329743377a63eedfcefa69
Felipe Gangrel <felipefrog@gmail.com>
2023-12-04 18:18:58 -0300

    Update Query type to accept unknown values

--- src/libs/navigation/utils.ts
+++ src/libs/navigation/utils.ts
@@ -1,9 +1,13 @@
-export type Query = Record<string, string>
+export type Query = Record<string, unknown>
 
 export const withQuery = (url: string, query: Query = {}) => {
   const queryString = Object.entries(query)
     .map(([key, value]) => `${key}=${value}`)
     .join('&')
+
+  if (!queryString) {
+    return url
+  }
 
   return `${url}?${queryString}`
 }
