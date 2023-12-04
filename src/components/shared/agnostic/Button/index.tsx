0c11f105edd6e6510fdc1002b451517358eb7cb8
Felipe Gangrel <felipefrog@gmail.com>
2023-12-04 19:37:02 -0300

    Add loading state to Button component

--- src/components/shared/agnostic/Button/index.tsx
+++ src/components/shared/agnostic/Button/index.tsx
@@ -3,16 +3,25 @@ import type { ButtonHTMLAttributes, FC } from 'react'
 import { cn } from '@/libs/styles'
 import { button, ButtonVariants } from '@/styles/buttons'
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariants
+import { ActivityIndicator } from '..'
+
+type Props = {
+  loading?: boolean
+} & ButtonHTMLAttributes<HTMLButtonElement> &
+  ButtonVariants
 
 export const Button: FC<Props> = ({
   className,
   size,
   color,
+  loading,
   type = 'button',
+  children,
   ...props
 }) => {
   return (
-    <button {...props} className={cn(button({ size, color }), className)} />
+    <button {...props} className={cn(button({ size, color }), className)}>
+      {loading ? <ActivityIndicator /> : children}
+    </button>
   )
 }
